2ae1f5373a70a975d5dd0b574542695ffcd3b1ee
Xin Chen <xinszchen@thoughtworks.com>
2018-09-04 00:19:43 +0800

    flatem

--- practices/flaten/double_dimensional_array_to_one_1.js
+++ practices/flaten/double_dimensional_array_to_one_1.js
@@ -3,6 +3,17 @@
 function double_to_one(collection) {
 
   //在这里写入代码
+  var ret = [];
+  collection.map(function(item){
+    if(item instanceof Array) {
+      item.map(function(innerItem) {
+        ret.push(innerItem);
+      })
+    }else {
+      ret.push(item);
+    }
+  });
+  return ret;
 }
 
 module.exports = double_to_one;
--- practices/flaten/double_dimensional_array_to_one_2.js
+++ practices/flaten/double_dimensional_array_to_one_2.js
@@ -3,6 +3,18 @@
 function double_to_one(collection) {
 
   //在这里写入代码
+  var ret = [];
+  collection.map(function(item) {
+    if (!(item instanceof Array)) {
+      item = [item];
+    }
+    item.map(function(innerItem) {
+      if (ret.indexOf(innerItem) === -1){
+        ret.push(innerItem);
+      }
+    })
+  })
+  return ret;
 }
 
 module.exports = double_to_one;
