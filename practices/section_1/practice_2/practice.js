b90dd02476ee1a86d4c650a5325de65f6afbe147
Xin Chen <xinszchen@thoughtworks.com>
2018-09-04 00:20:18 +0800

    section 1

--- practices/section_1/practice_2/practice.js
+++ practices/section_1/practice_2/practice.js
@@ -1,5 +1,8 @@
 function collect_same_elements(collection_a, collection_b) {
   //在这里写入代码
+  return collection_a.filter((item)=>{
+    return collection_b.map((innerArray) => innerArray.includes(item)).includes(true);
+  })
 }
 
 module.exports = collect_same_elements;
