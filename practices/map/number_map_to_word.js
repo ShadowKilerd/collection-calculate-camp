21e0ffb5b730ab53418735a015d635d0f51de838
Xin Chen <xinszchen@thoughtworks.com>
2018-09-04 00:19:34 +0800

    map

--- practices/map/number_map_to_word.js
+++ practices/map/number_map_to_word.js
@@ -1,6 +1,8 @@
 'use strict';
 var number_map_to_word = function(collection){
-  return ['a','b','c','d','e'];
+  return collection.map(function(number){
+    return String.fromCharCode(number+96);
+  })
 };
 
 module.exports = number_map_to_word;
--- practices/map/number_map_to_word_over_26.js
+++ practices/map/number_map_to_word_over_26.js
@@ -1,6 +1,29 @@
 'use strict';
+function numberToAsciiMapper(number) {
+  return String.fromCharCode(number + 96)
+}
+
+function calculateTimesAndChar(number) {
+  var times = parseInt(number / 26);
+  var char = (number % 26);
+  if(char == 0) {
+    return [times - 1, 26];
+  }
+  return [times, char];
+}
+function chars(number){
+  var ret = calculateTimesAndChar(number);
+  if(ret[0] != 0) {
+    return numberToAsciiMapper(ret[0]) + numberToAsciiMapper(ret[1]);
+  }else {
+    return numberToAsciiMapper(ret[1]);
+  }
+}
+
 var number_map_to_word_over_26 = function(collection){
-  return ['a','m','aa','ad','y','aa'];
+  return collection.map(function(number) {
+    return chars(number);
+  })
 };
 
 module.exports = number_map_to_word_over_26;
