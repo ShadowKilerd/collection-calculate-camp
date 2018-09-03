48e9fffc5ccb99b28c56cb05a82ca99bca62840c
Xin Chen <xinszchen@thoughtworks.com>
2018-09-04 00:20:08 +0800

    ruduce

--- practices/ruduce/split_to_zero.js
+++ practices/ruduce/split_to_zero.js
@@ -2,6 +2,14 @@
 
 function spilt_to_zero(number, interval) {
   //在这里写入代码
+  let ret = [number];
+  while(number > 0) {
+    number -= interval;
+    number = Number.parseFloat(number.toFixed(1));
+    ret.push(number);
+
+  }
+  return ret;
 }
 
 module.exports = spilt_to_zero;
